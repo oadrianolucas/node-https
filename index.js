db7da266809dbeab86ec02b2dd235879bf6ea0c9
Adrino Lucas <contato@adrianolucas.com>
2022-04-22 16:18:07 -0300

    new ssl

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+//const http = require("http");
 const https = require('https')
 const fs = require("fs");
 const express = require("express");
@@ -8,23 +8,22 @@ app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-
+/* 
 http.createServer(app).listen(80, ()=>{
     console.log('Está rodando na porta 80')
 });
-/*
+*/
 https
   .createServer(
     {
-      key: fs.readFileSync("keys/key.pem"),
-      cert: fs.readFileSync("keys/cert.pem"),
+      key: fs.readFileSync("/etc/letsencrypt/live/block4vars.com/privkey.pem"),
+      cert: fs.readFileSync("/etc/letsencrypt/live/block4vars.com/fullchain.pem"),
     },
     app
   )
   .listen(80, () => {
     console.log("Está rodando na porta 80");
   });
-*/
 
 app.get('/',(req, res)=>{
     res.render('index.html')
